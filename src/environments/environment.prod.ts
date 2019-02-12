45f091b311dc60c0b9b7b955c4dabefd51b584c8
Boban <bbnmarkovic@gmail.com>
2019-02-12 19:21:40 +0100

    Add API address

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,15 @@
 export const environment = {
-  production: true
+  production: true,
+  API_BASE_URL: 'https://api.dev.globalitfactory.eu/v1',
+  authConfig: {
+    authority: 'https://demo.identityserver.io',
+    // authority: 'http://identitymanagementapi-1966185121.ap-southeast-2.elb.amazonaws.com',
+    client_id: 'implicit',
+    redirect_uri: 'http://localhost:4200/auth-callback',
+    post_logout_redirect_uri: 'http://localhost:4200/',
+    response_type: "id_token token",
+    scope: "openid profile",
+    filterProtocolClaims: true,
+    loadUserInfo: true
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,9 @@
 export const environment = {
   production: false,
+  API_BASE_URL: 'https://api.dev.globalitfactory.eu/v1',
   authConfig: {
     authority: 'https://demo.identityserver.io',
+    // authority: 'http://identitymanagementapi-1966185121.ap-southeast-2.elb.amazonaws.com',
     client_id: 'implicit',
     redirect_uri: 'http://localhost:4200/auth-callback',
     post_logout_redirect_uri: 'http://localhost:4200/',

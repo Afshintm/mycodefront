f3df2638a6ec2ed3dee8422ef0edf980a78c2f0c
Hien Thuong <hien.thuong@homestay.care>
2019-02-13 11:04:23 +1100

    card for daily activities

--- src/app/redux/selectors/app.selector.ts
+++ src/app/redux/selectors/app.selector.ts
@@ -4,3 +4,9 @@ import { IAppState } from '../app.state';
 const appState = (state: IAppState) => state;
 
 export const configurationSelector = createSelector(appState, (state: IAppState) => state.configuration);
+
+export const someDataSelector = createSelector(appState, (state: IAppState) => {
+  // console.log("state", state);
+  // console.log("state.someData", state.someData);
+  return state.someData;
+});

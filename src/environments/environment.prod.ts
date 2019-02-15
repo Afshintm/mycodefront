30f9eb4add74ae2325cd38d6c26f831630ce6678
Afshin Teymoori <afshin.teymoori@homestay.care>
2019-02-15 18:52:54 +1100

    aws deploy setup

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,13 +2,12 @@ export const environment = {
   production: true,
   API_BASE_URL: 'http://essence-communication-api-791146997.ap-southeast-2.elb.amazonaws.com/api',
   authConfig: {
-    authority: 'https://demo.identityserver.io',
-    // authority: 'http://identitymanagementapi-1966185121.ap-southeast-2.elb.amazonaws.com',
-    client_id: 'implicit',
-    redirect_uri: 'http://localhost:4200/auth-callback',
-    post_logout_redirect_uri: 'http://localhost:4200/',
+    authority: 'http://identitymanagementapi-1966185121.ap-southeast-2.elb.amazonaws.com',
+    client_id: 'angularclient-aws',
+    redirect_uri: 'http://app.homestay.care/auth-callback',
+    post_logout_redirect_uri: 'http://app.homestay.care/',
     response_type: "id_token token",
-    scope: "openid profile",
+    scope: "openid profile email roles api1",
     filterProtocolClaims: true,
     loadUserInfo: true
   }

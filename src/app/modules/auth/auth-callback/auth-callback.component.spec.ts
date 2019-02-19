edfa583f4ba8836c221391669d10a0ecd1bdf62c
Boban <bbnmarkovic@gmail.com>
2019-02-19 23:14:31 +0100

    Fix unit test

--- src/app/modules/auth/auth-callback/auth-callback.component.spec.ts
+++ src/app/modules/auth/auth-callback/auth-callback.component.spec.ts
@@ -1,14 +1,28 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, inject, TestBed } from '@angular/core/testing';
 
 import { AuthCallbackComponent } from './auth-callback.component';
+import { AuthService } from '../services/auth.service';
+import { Router } from '@angular/router';
+
+
 
 describe('AuthCallbackComponent', () => {
   let component: AuthCallbackComponent;
   let fixture: ComponentFixture<AuthCallbackComponent>;
+  const mockAuthService = {
+    completeAuthentication: jasmine.createSpy().and.returnValue(Promise.resolve())
+  };
+  const mockRouter = {
+    navigate: jasmine.createSpy('navigate')
+  };
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ AuthCallbackComponent ]
+      declarations: [ AuthCallbackComponent ],
+      providers: [
+        { provide: Router, useValue: mockRouter },
+        { provide: AuthService, useValue: mockAuthService }
+      ]
     })
     .compileComponents();
   }));

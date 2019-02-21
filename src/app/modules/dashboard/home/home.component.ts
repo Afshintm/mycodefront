ad304f578bfae85dfd363aad5872848872273192
Hien Thuong <hien.thuong@homestay.care>
2019-02-21 14:41:14 +1100

    aws sdk for js

--- src/app/modules/dashboard/home/home.component.ts
+++ src/app/modules/dashboard/home/home.component.ts
@@ -1,3 +1,5 @@
+/// <reference types="aws-sdk" />
+
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { untilDestroyed } from 'ngx-take-until-destroy';
 import { DashboardService } from '../dashboard.service';
@@ -7,6 +9,10 @@ import { Store } from '@ngrx/store';
 import { personSelector } from '../../../redux/selectors/app.selector';
 import { dailyActivitySelector } from '../../../redux/selectors/app.selector';
 import { IDailyActivity } from '../../../models/daily-activity';
+import * as AWS from "../../../../../node_modules/aws-sdk";
+
+const sns = new AWS.SNS();
+
 
 @Component({
   selector: 'app-home',
@@ -29,6 +35,16 @@ export class HomeComponent implements OnInit, OnDestroy {
     this.store.select(dailyActivitySelector)
       .pipe(untilDestroyed(this))
       .subscribe((dailyActivities: IDailyActivity[]) => this.dailyActivities = dailyActivities);
+
+    // you don't just subscribe to "news", but the whole Amazon Resource Name (ARN)
+    sns.subscribe({
+      Protocol: 'http',
+      TopicArn: 'arn:aws:sns:ap-southeast-2:590986216309:SomeTestTopicForSns',
+      Endpoint: '//your-endpoint-url.com'
+    }, (error, data) => {
+      console.log(error || data);
+    });
+
 
   }
 
